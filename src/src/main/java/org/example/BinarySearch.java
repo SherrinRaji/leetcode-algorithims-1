87fb2d2327a5da629f2455d4e1269a95c69d6413
Sherrin Raji <sh810841@dal.ca>
2023-01-03 00:46:15 -0500

    day1

--- src/src/main/java/org/example/BinarySearch.java
+++ src/src/main/java/org/example/BinarySearch.java
@@ -1,7 +1,43 @@
 package org.example;
 
+// Big O(logn)
 public class BinarySearch {
+/**   Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ *    You must write an algorithm with O(log n) runtime complexity. **/
+    private int searchInsert(int[] nums, int target) {
+        int mid=0,left=0,right = nums.length-1;
+
+        while(left<=right){
+            mid = (left+right)/2; // avoid integer overflow -> left + (right - left)/2
+
+            if(target == nums[mid])
+                return mid;
+            else if(target>nums[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+
+        }
+        return left;
+    }
+    private int search(int[] nums, int target) {
+        int mid,left=0,right = nums.length-1;
+
+        while(left<=right){
+            mid = (left+right)/2; // avoid integer overflow -> left + (right - left)/2
+
+            if(target == nums[mid])
+                return mid;
+            else if(target>nums[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+
+        }
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinarySearch bs = new BinarySearch();
+        System.out.println(bs.search(new int[]{-1,0,3,5,9,12},9));
     }
 }
